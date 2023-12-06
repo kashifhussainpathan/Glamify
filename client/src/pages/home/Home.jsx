529dc7e5d65ec6bc3032e4a53db574e4b84b9b8a
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-12-06 19:34:17 +0530

    created pages

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HomeSlider from "./components/HomeSlider";
+import OurBenifits from "./components/OurBenifits";
+
+const Home = () => {
+  return (
+    <div>
+      <HomeSlider />
+      <OurBenifits />
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/pages/profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Signin, Signup } from "../../components";
+
+const Profile = () => {
+  return (
+    <div>
+      <Signup />
+      {/* <Signin /> */}
+    </div>
+  );
+};
+
+export default Profile;
