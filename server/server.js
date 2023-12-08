1503f3d2df928d42af2f12405fe2863612cea01e
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-12-08 15:19:17 +0530

    refactored

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ import express from "express";
 import cookieParser from "cookie-parser";
 // import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-// import listingRouter from "./routes/listing.route.js";
 
 dotenv.config({ path: "./config.env" });
 const PORT = process.env.PORT || 8000;
@@ -23,13 +22,6 @@ app.listen(PORT, () => {
 
 // app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-// app.use("/api/listing", listingRouter);
-
-// app.use(express.static(path.join(__dirname, "/client/dist")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
