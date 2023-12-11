04736598ce4bca3651189215b7971b52fc3dd121
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-12-11 20:48:37 +0530

    added few styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
       screens: {
         xs: "320px",
         sm: "375px",
@@ -13,10 +17,7 @@ export default {
         lgl: "1024px",
         xl: "1280px",
       },
-      fontFamily: {
-        bodyFont: ["Poppins", "sans-serif"],
-        titleFont: ["Montserrat", "sans-serif"],
-      },
+
       colors: {
         bodyColor: "#212428",
         lightText: "#c4cfde",
@@ -27,6 +28,20 @@ export default {
         small: "0px 4px 8px rgba(35, 38, 59, 0.25)",
         medium: "0px 8px 16px rgba(35, 38, 59, 0.25)",
         large: "0px 12px 32px rgba(35, 38, 59, 0.25)",
+      },
+      transitionDuration: {
+        DEFAULT: "300ms",
+        2000: "2000ms",
+      },
+      transitionTimingFunction: {
+        "in-expo": "cubic-bezier(0.7, 0, 0.84, 0)",
+        "out-expo": "cubic-bezier(0.16, 1, 0.3, 1)",
+        "in-out-expo": "cubic-bezier(0.87, 0, 0.13, 1)",
+      },
+      strokeWidth: {
+        3: "3",
+        4: "4",
+        5: "5",
       },
     },
   },
