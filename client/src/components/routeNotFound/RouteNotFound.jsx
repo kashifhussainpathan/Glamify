06de84984d769e3557783880aec88853cb786360
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-12-11 20:40:05 +0530

    created routeNotFound page

---
+++ client/src/components/routeNotFound/RouteNotFound.jsx
@@ -0,0 +1,14 @@
+const RouteNotFound = () => {
+  return (
+    <div className="flex items-center justify-center h-[90vh]">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
+        <p className="text-lg">
+          Oops! The page you are looking for does not exist.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default RouteNotFound;
