8045c4cae0bf4d72f69bb2303edff99fe60b0fb8
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-12-11 20:39:34 +0530

    added productsReducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import userReducer from "./user/userSlice";
+import productsReducer from "./products/productsSlice";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  products: productsReducer,
 });
 
 const store = configureStore({ reducer: rootReducer });
