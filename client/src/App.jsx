c11dd7a66d190fcdcd96551c7451a36466edb0d1
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-12-11 20:46:17 +0530

    added lazy loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,37 @@
 import "./App.css";
-import { Navbar } from "./components";
-import { useSelector } from "react-redux";
+import { lazy, Suspense, useEffect, useMemo } from "react";
 import { Route, Routes } from "react-router-dom";
-import { Cart, Home, Products, Profile } from "./pages";
+
+import { getMenProducts } from "./redux";
+import { useDispatch } from "react-redux";
+import { Navbar, RouteNotFound } from "./components";
+
+const Home = lazy(() => import("./pages/home/Home"));
+const Cart = lazy(() => import("./pages/cart/Cart"));
+const Profile = lazy(() => import("./pages/profile/Profile"));
+const Products = lazy(() => import("./pages/products/Products"));
 
 function App() {
-  const user = useSelector(({ user }) => user.currentUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMenProducts());
+  }, []);
 
   return (
-    <div className="bg-slate-50 h-screen">
+    <div className="text-gray-800">
       <Navbar />
 
       <div className="app-routes">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/products" element={<Products />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="*" element={<RouteNotFound />} />
+          </Routes>
+        </Suspense>
       </div>
     </div>
   );
