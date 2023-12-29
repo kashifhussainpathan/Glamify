1a4d5558cc4c868aa33faa329c6b663cdb05767c
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-12-29 15:21:17 +0530

    changed auth folder

---
+++ client/src/components/auth/Signin.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { signinAsync } from "@redux";
+import { useUserState } from "@hooks";
+import { Button } from "@components/@ui";
+import { signInInputs } from "@constants";
+import FormInput from "../formInput/FormInput";
+
+const Signin = () => {
+  const dispatch = useDispatch();
+  const { error } = useUserState();
+
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(signinAsync(values));
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="flex items-center justify-center">
+      <div className="bg-white p-6 rounded shadow-small">
+        <form onSubmit={handleSubmit} className="flex flex-col items-center">
+          <div className="text-2xl mt-[-10px] mb-1 font-semibold ">Sign In</div>
+          {signInInputs.map((input) => (
+            <FormInput
+              key={input.id}
+              {...input}
+              value={values[input.name] || ""}
+              onChange={onChange}
+            />
+          ))}
+
+          <Button className="w-full mt-2">SIGN IN</Button>
+        </form>
+        {error && <p className="mt-4 text-red-500 text-center">{error}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
---
+++ client/src/components/auth/Signup.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { inputs } from "@constants";
+import { useUserState } from "@hooks";
+import { signupAsync } from "@redux";
+import { Button } from "@components/@ui";
+import FormInput from "../formInput/FormInput";
+
+const Signup = () => {
+  const dispatch = useDispatch();
+  const { error } = useUserState();
+
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    avatar: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { confirmPassword, ...userDetails } = values;
+    dispatch(signupAsync(userDetails));
+  };
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+
+    setValues({ ...values, [name]: value });
+  };
+
+  const dynamicInputs = inputs.map((input) => {
+    if (input.name === "confirmPassword") {
+      return {
+        ...input,
+        pattern: values.password,
+      };
+    }
+    return input;
+  });
+
+  return (
+    <div className="flex items-center justify-center">
+      <div className="bg-white p-6 rounded shadow-small">
+        <form onSubmit={handleSubmit} className="flex flex-col items-center">
+          <div className="text-2xl mt-[-10px] mb-1 font-semibold ">
+            Register
+          </div>
+          {dynamicInputs.map((input) => (
+            <FormInput
+              key={input.id}
+              {...input}
+              value={values[input.name]}
+              onChange={onChange}
+            />
+          ))}
+
+          <Button className="w-full mt-2">REGISTER</Button>
+        </form>
+
+        {error && <p className="mt-4 text-red-500 text-center">{error}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
