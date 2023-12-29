62d56a0c4150e5dcc5423764291967a6063d627d
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-12-29 15:21:37 +0530

    imported components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,45 @@
-import Signup from "./signup/Signup";
+import Signin from "./auth/Signin";
+import Signup from "./auth/Signup";
 import Navbar from "./navbar/Navbar";
-import Signin from "./signin/Signin";
-import ErrorFallback from "./ErrorFallback";
+import Footer from "./footer/Footer";
+import Address from "./profile/Address";
+import Filters from "./filters/Filters";
+import HomeSlider from "./home/HomeSlider";
+import CartLoader from "./cart/CartLoader";
+import HomeHeading from "./home/HomeHeading";
 import FormInput from "./formInput/FormInput";
+import OrderSummary from "./cart/OrderSummary";
+import ProfileCard from "./profile/ProfileCard";
+import ProductCard from "./products/ProductCard";
+import Pagination from "./pagination/Pagination";
+import ProductLoader from "./products/ProductLoader";
+import CartProductCard from "./cart/CartProductCard";
+import ProductDetails from "./products/ProductDetails";
+import RouteNotFound from "./routeNotFound/RouteNotFound";
+import ErrorFallback from "./errorFallback/ErrorFallback";
+import ProductsCarousel from "./products/ProductsCarousel";
+import ToggleSigninSignup from "./profile/ToggleSigninSignup";
 
-export { FormInput, ErrorFallback, Signup, Navbar, Signin };
+export {
+  Signup,
+  Footer,
+  Navbar,
+  Signin,
+  Address,
+  Filters,
+  FormInput,
+  Pagination,
+  CartLoader,
+  HomeSlider,
+  HomeHeading,
+  ProductCard,
+  ProfileCard,
+  OrderSummary,
+  ErrorFallback,
+  ProductLoader,
+  RouteNotFound,
+  ProductDetails,
+  CartProductCard,
+  ProductsCarousel,
+  ToggleSigninSignup,
+};
