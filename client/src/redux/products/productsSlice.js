baeba23596e4b1ee44c5d09c7eb65e121feb251e
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-12-29 15:38:16 +0530

    added new functions

--- client/src/redux/products/productsSlice.js
+++ client/src/redux/products/productsSlice.js
@@ -1,24 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getMenProducts } from "./productsThunk";
+import {
+  getMenProducts,
+  getProduct,
+  getProducts,
+  getSimilarProducts,
+  getWomenProducts,
+} from "./productsThunk";
 
 const initialState = {
-  totalProducts: [],
+  error: "",
+  product: {},
+  products: [],
+  status: "idle",
+  totalProducts: "",
   menProducts: [],
   womenProducts: [],
-  status: "idle",
-  error: "",
+  currentPage: 1,
+  similarProducts: [],
 };
 
 const productsSlice = createSlice({
   name: "products",
   initialState,
   reducers: {
-    setProducts: (state, action) => {
-      state.products = action.payload;
+    setCurrentPage: (state, action) => {
+      state.currentPage = action.payload;
     },
   },
 
   extraReducers: (builder) => {
+    builder.addCase(getProducts.pending, (state) => {
+      state.status = "loading";
+    });
+
+    builder.addCase(getProducts.fulfilled, (state, action) => {
+      state.products = action.payload.data;
+      state.totalProducts = action.payload?.pageInfo?.totalItems;
+      state.error = "";
+      state.status = "success";
+    });
+
+    builder.addCase(getProducts.rejected, (state, action) => {
+      state.status = "error";
+      state.error = action.payload;
+    });
+
     builder.addCase(getMenProducts.pending, (state) => {
       state.status = "loading";
     });
@@ -26,11 +52,55 @@ const productsSlice = createSlice({
     builder.addCase(getMenProducts.fulfilled, (state, action) => {
       state.error = "";
       state.status = "success";
-      state.totalProducts = action.payload?.pageInfo?.totalItems;
       state.menProducts = action.payload.data;
     });
 
     builder.addCase(getMenProducts.rejected, (state, action) => {
+      state.status = "error";
+      state.error = action.payload;
+    });
+
+    builder.addCase(getWomenProducts.pending, (state) => {
+      state.status = "loading";
+    });
+
+    builder.addCase(getWomenProducts.fulfilled, (state, action) => {
+      state.error = "";
+      state.status = "success";
+      state.womenProducts = action.payload.data;
+    });
+
+    builder.addCase(getWomenProducts.rejected, (state, action) => {
+      state.status = "error";
+      state.error = action.payload;
+    });
+
+    builder.addCase(getProduct.pending, (state) => {
+      state.status = "loading";
+    });
+
+    builder.addCase(getProduct.fulfilled, (state, action) => {
+      state.error = "";
+      state.status = "success";
+      state.product = action.payload;
+    });
+
+    builder.addCase(getProduct.rejected, (state, action) => {
+      state.status = "error";
+      state.error = action.payload;
+    });
+
+    builder.addCase(getSimilarProducts.pending, (state) => {
+      state.status = "loading";
+    });
+
+    builder.addCase(getSimilarProducts.fulfilled, (state, action) => {
+      state.error = "";
+      state.status = "success";
+      state.similarProducts = action.payload?.data?.products;
+    });
+
+    builder.addCase(getSimilarProducts.rejected, (state, action) => {
       state.status = "error";
       state.error = action.payload;
     });
@@ -39,4 +109,4 @@ const productsSlice = createSlice({
 
 export default productsSlice.reducer;
 
-export const { setProducts } = productsSlice.actions;
+export const { setCurrentPage } = productsSlice.actions;
--- client/src/redux/products/productsThunk.js
+++ client/src/redux/products/productsThunk.js
@@ -3,9 +3,25 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 const BASE_URL = "http://localhost:4000";
 
+export const getProducts = createAsyncThunk(
+  "products/getProducts",
+  async ({ page, gender, filters }, { rejectWithValue }) => {
+    try {
+      console.log(filters);
+      const response = await axios.get(
+        `${BASE_URL}/api/products/filteredProducts/${gender}/${page}`,
+        { params: { filters } }
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
 export const getMenProducts = createAsyncThunk(
   "products/getMenProducts",
-  async (page, { rejectWithValue }, gender = "men") => {
+  async ({ page, gender = "men" }, { rejectWithValue }) => {
     try {
       const response = await axios.get(
         `${BASE_URL}/api/products/${gender}/${page}`
@@ -19,7 +35,7 @@ export const getMenProducts = createAsyncThunk(
 
 export const getWomenProducts = createAsyncThunk(
   "products/getWomenProducts",
-  async (page, { rejectWithValue }, gender = "women") => {
+  async ({ page, gender = "women" }, { rejectWithValue }) => {
     try {
       const response = await axios.get(
         `${BASE_URL}/api/products/${gender}/${page}`
@@ -27,6 +43,32 @@ export const getWomenProducts = createAsyncThunk(
       return response.data;
     } catch (error) {
       return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const getProduct = createAsyncThunk(
+  "products/getProduct",
+  async (productId, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(`${BASE_URL}/api/products/${productId}`);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const getSimilarProducts = createAsyncThunk(
+  "prodducts/getSimilarProducts",
+  async ({ gender, product_type }, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}/api/products/similarProducts/${gender}/${product_type}`
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
   }
 );
