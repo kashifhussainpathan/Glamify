673ca868667f348db28be0436c916956dbfae2c6
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-12-29 15:28:54 +0530

    added ref id

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
@@ -20,6 +20,9 @@ const userSchema = new mongoose.Schema(
       type: String,
       default:
         "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+    cart: {
+      type: [mongoose.Schema.Types.ObjectId],
     },
   },
   { timestamps: true }
