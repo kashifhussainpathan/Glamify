3c46c18f6391a0c6ddb4787eb269441951d9bba6
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-12-29 15:31:10 +0530

    added cart & filters reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,14 @@
-import userReducer from "./user/userSlice";
-import productsReducer from "./products/productsSlice";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
+import userReducer from "./user/userSlice";
+import cartReducer from "./cart/cartSlice";
+import filtersReducer from "./filters/filtersSlice";
+import productsReducer from "./products/productsSlice";
+
 const rootReducer = combineReducers({
+  cart: cartReducer,
   user: userReducer,
+  filters: filtersReducer,
   products: productsReducer,
 });
 
