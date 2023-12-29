59433b5ee19def40a369d40f6cd04c2e28884e24
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-12-29 15:43:32 +0530

    refactored

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -56,10 +56,7 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
 
-    res
-      .cookie("access_token", token, { httpOnly: true })
-      .status(200)
-      .json(rest);
+    res.status(200).json({ user: rest, token });
   } catch (error) {
     next(error);
   }
@@ -67,7 +64,6 @@ export const signin = async (req, res, next) => {
 
 export const signOut = async (req, res, next) => {
   try {
-    res.clearCookie("access_token");
     res.status(200).json("User has been logged out!");
   } catch (error) {
     next(error);
