55e4435d3023a33c6a5fc35edf98c087503d679b
KashifPathan <kashifpathan.kp.kp@gmail.com>
2023-12-29 15:20:34 +0530

    refactored

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
+
 import "./index.css";
 import App from "./App.jsx";
 import store from "./redux/store.js";
-import { Provider } from "react-redux";
-import { ErrorFallback } from "./components";
-import { ErrorBoundary } from "react-error-boundary";
-import { BrowserRouter } from "react-router-dom";
+import { ErrorFallback } from "@components";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ErrorBoundary fallbackRender={ErrorFallback}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </ErrorBoundary>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <ErrorBoundary fallbackRender={ErrorFallback}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ErrorBoundary>
+  /* </React.StrictMode> */
 );
