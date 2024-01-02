8bbe8514d2f40434877828e112e52f3d0f2f05df
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-01-02 14:18:47 +0530

    added get route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,10 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Glamify");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}! `);
 });
