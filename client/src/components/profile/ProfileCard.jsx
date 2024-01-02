3f7da54853e66a3c54f1d2ed704d5d6113f75484
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-01-02 12:34:21 +0530

    added loading

--- client/src/components/profile/ProfileCard.jsx
+++ client/src/components/profile/ProfileCard.jsx
@@ -1,22 +1,19 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
-
-import { Button } from "../@ui";
+import { Button, Skeleton } from "../@ui";
 import { updateUserDetails } from "@redux";
 import { email, username } from "@constants";
 import { useUserState, useToken } from "@hooks";
+import FormInput from "../formInput/FormInput";
 
 import { FaEdit, FaSave } from "react-icons/fa";
-import FormInput from "../formInput/FormInput";
 
 const ProfileCard = () => {
   const dispatch = useDispatch();
 
   const { token } = useToken();
-  const { status, currentUser: user } = useUserState();
+  const { updateUserDetailsStatus, currentUser: user } = useUserState();
 
   const [isEditing, setIsEditing] = useState(false);
   const [updatedDetails, setUpdatedDetails] = useState({});
@@ -38,66 +35,77 @@ const ProfileCard = () => {
     setIsEditing(false);
   };
 
+  const handleLogoutClick = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+
   return (
-    <div className="">
-      <div className="">
-        <div>
-          <div className="w-full h-[200px] max-md:h-[100px]">
-            <img
-              src="https://plchldr.co/i/500x250?bg=bebebe&text=Cover-Image"
-              alt="cover-image"
-              className="w-full h-full object-cover rounded"
-            />
-          </div>
+    <div>
+      <div>
+        <div className="w-full h-[200px] max-md:h-[100px]">
           <img
-            src={user?.avatar}
-            alt="User Avatar"
-            className="w-[150px] h-[150px] rounded-full mt-[-70px] mx-auto max-md:h-[70px] max-md:w-[70px] max-md:mt-[-35px]"
+            src="https://plchldr.co/i/500x250?bg=bebebe&text=Cover-Image"
+            alt="cover-image"
+            className="w-full h-full object-cover rounded"
           />
         </div>
+        <img
+          src={user?.avatar}
+          alt="User Avatar"
+          className="w-[150px] h-[150px] rounded-full mt-[-70px] mx-auto max-md:h-[70px] max-md:w-[70px] max-md:mt-[-35px]"
+        />
+      </div>
 
-        <div className="flex flex-col gap-1 items-center justify-center mt-3">
-          {isEditing ? (
-            <div>
-              <FormInput
-                {...username}
-                value={updatedDetails?.username}
-                onChange={handleUserDetailsChange}
-              />
-            </div>
-          ) : (
-            <h3 className="text-lg font-semibold">
-              {status === "loading" ? <Skeleton width={100} /> : user?.username}
-            </h3>
-          )}
-          {isEditing ? (
-            <div>
-              <FormInput
-                {...email}
-                value={updatedDetails?.email}
-                onChange={handleUserDetailsChange}
-              />
-            </div>
-          ) : (
-            <p className="text-gray-500">
-              {status === "loading" ? <Skeleton width={100} /> : user?.email}
-            </p>
-          )}
-        </div>
+      <div className="flex flex-col gap-1 items-center justify-center mt-3">
+        {isEditing ? (
+          <div>
+            <FormInput
+              {...username}
+              value={updatedDetails?.username}
+              onChange={handleUserDetailsChange}
+            />
+          </div>
+        ) : (
+          <h3 className="text-lg font-semibold">
+            {updateUserDetailsStatus === "loading" ? (
+              <Skeleton classes="h-5 w-32" />
+            ) : (
+              user?.username
+            )}
+          </h3>
+        )}
+        {isEditing ? (
+          <div>
+            <FormInput
+              {...email}
+              value={updatedDetails?.email}
+              onChange={handleUserDetailsChange}
+            />
+          </div>
+        ) : (
+          <p className="text-gray-500">
+            {updateUserDetailsStatus === "loading" ? (
+              <Skeleton classes="h-4 w-32" />
+            ) : (
+              user?.email
+            )}
+          </p>
+        )}
+      </div>
 
-        <div className="flex justify-center mt-4">
-          {isEditing ? (
-            <Button onClick={handleSaveClick} className="flex items-center">
-              <FaSave className="mr-2" /> <span> Save </span>
-            </Button>
-          ) : (
-            <Button onClick={handleEditClick} className="flex items-center">
-              <FaEdit className="mr-2" /> <span>Edit </span>
-            </Button>
-          )}
+      <div className="flex justify-center mt-4">
+        {isEditing ? (
+          <Button onClick={handleSaveClick} className="flex items-center">
+            <FaSave className="mr-2" /> <span> Save </span>
+          </Button>
+        ) : (
+          <Button onClick={handleEditClick} className="flex items-center">
+            <FaEdit className="mr-2" /> <span>Edit </span>
+          </Button>
+        )}
 
-          <Button onClick={handleEditClick}>Logout</Button>
-        </div>
+        <Button onClick={handleLogoutClick}>Logout</Button>
       </div>
     </div>
   );
