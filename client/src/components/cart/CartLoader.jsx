6859ced8f880496372be9d2bd73ebe92745cbe10
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-01-02 12:26:19 +0530

    fixed skeleton loader

--- client/src/components/cart/CartLoader.jsx
+++ client/src/components/cart/CartLoader.jsx
@@ -1,247 +1,99 @@
 import React from "react";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Skeleton } from "@ui";
 
-const CartLoader = () => {
+const CartCardLoader = () => {
   return (
-    <div className="flex flex-wrap justify-center gap-8 my-10 items-start">
-      {/* Cart Product Card */}
-      <div>
-        <div className="w-[430px] h-auto flex shadow gap-4 rounded-sm pl-2 relative mb-4">
-          <div className="my-2 pl-1">
-            <Skeleton height={150} width={100} />
+    <div>
+      <div className="w-[450px] max-md:w-[320px] h-auto flex shadow gap-4 rounded-sm pl-2 relative py-2 mb-4">
+        <Skeleton classes="h-[145px] w-[120px] max-md:w-[80px] max-md:h-[120px] max-md:my-auto" />
+
+        <div className="my-2">
+          <Skeleton classes="h-5 w-50 max-md:h-4 max-md:w-32 mb-2" />
+          <Skeleton classes="h-4 w-32 max-md:h-4 max-md:w-16 mb-2" />
+          <Skeleton classes="h-5 w-12 max-md:h-4 max-md:w-12" />
+
+          <div className="flex items-center max-md:text-sm my-2 gap-1">
+            <Skeleton classes="h-3 w-3 max-md:h-2 max-md:w-2" />
+            <Skeleton classes="h-3 w-3 max-md:h-2 max-md:w-2" />
+            <Skeleton classes="h-3 w-3 max-md:h-2 max-md:w-2" />
+            <Skeleton classes="h-3 w-3 max-md:h-2 max-md:w-2" />
+            <Skeleton classes="h-3 w-3 max-md:h-2 max-md:w-2" />
+
+            <Skeleton classes="h-3 w-4 max-md:h-2 max-md:w-2" />
           </div>
 
-          <div className="my-2">
-            <p className="text-lg font-semibold">
-              <Skeleton height={20} width={200} />
-            </p>
-            <p className="font-semibold">
-              <Skeleton height={15} width={80} />
-            </p>
-            <p className="font-semibold text-lg">
-              <Skeleton height={15} width={30} />
-            </p>
-            <div className="flex items-center my-1 gap-1">
-              {Array.from({ length: 5 }).map((_, i) => (
-                <Skeleton height={12} width={12} key={i} />
-              ))}
-              <span className="pl-1 text-sm">
-                {" "}
-                <Skeleton height={10} width={20} />
-              </span>
-            </div>
+          <div className="flex justify-between w-[270px] max-md:w-full max-md:gap-4">
+            <select
+              name="size"
+              className=" cursor-pointer border my-1 max-md:text-sm"
+            >
+              <option value="">Choose Size</option>
+            </select>
 
-            <div className="flex justify-between w-[270px]">
-              <Skeleton height={20} width={80} />
-
-              <div className="flex justify-center gap-1">
-                <button className="px-[10px] pb-[3px] border rounded hover:bg-gray-200 text-xl">
-                  -
-                </button>
-                <input
-                  name="quantity"
-                  type="text"
-                  placeholder="1"
-                  numeric="true"
-                  className=" w-6 text-center"
-                />
-                <button className="px-2 pb-[1px] border rounded hover:bg-gray-200 text-lg">
-                  +
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-[430px] h-auto flex shadow gap-4 rounded-sm pl-2 relative mb-4">
-          <div className="my-2 pl-1">
-            <Skeleton height={150} width={100} />
-          </div>
-
-          <div className="my-2">
-            <p className="text-lg font-semibold">
-              <Skeleton height={20} width={200} />
-            </p>
-            <p className="font-semibold">
-              <Skeleton height={15} width={80} />
-            </p>
-            <p className="font-semibold text-lg">
-              <Skeleton height={15} width={30} />
-            </p>
-            <div className="flex items-center my-1 gap-1">
-              {Array.from({ length: 5 }).map((_, i) => (
-                <Skeleton height={12} width={12} key={i} />
-              ))}
-              <span className="pl-1 text-sm">
-                {" "}
-                <Skeleton height={10} width={20} />
-              </span>
-            </div>
-
-            <div className="flex justify-between w-[270px]">
-              <Skeleton height={20} width={80} />
-
-              <div className="flex justify-center gap-1">
-                <button className="px-[10px] pb-[3px] border rounded hover:bg-gray-200 text-xl">
-                  -
-                </button>
-                <input
-                  name="quantity"
-                  type="text"
-                  placeholder="1"
-                  numeric="true"
-                  className=" w-6 text-center"
-                />
-                <button className="px-2 pb-[1px] border rounded hover:bg-gray-200 text-lg">
-                  +
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-[430px] h-auto flex shadow gap-4 rounded-sm pl-2 relative mb-4">
-          <div className="my-2 pl-1">
-            <Skeleton height={150} width={100} />
-          </div>
-
-          <div className="my-2">
-            <p className="text-lg font-semibold">
-              <Skeleton height={20} width={200} />
-            </p>
-            <p className="font-semibold">
-              <Skeleton height={15} width={80} />
-            </p>
-            <p className="font-semibold text-lg">
-              <Skeleton height={15} width={30} />
-            </p>
-            <div className="flex items-center my-1 gap-1">
-              {Array.from({ length: 5 }).map((_, i) => (
-                <Skeleton height={12} width={12} key={i} />
-              ))}
-              <span className="pl-1 text-sm">
-                {" "}
-                <Skeleton height={10} width={20} />
-              </span>
-            </div>
-
-            <div className="flex justify-between w-[270px]">
-              <Skeleton height={20} width={80} />
-
-              <div className="flex justify-center gap-1">
-                <button className="px-[10px] pb-[3px] border rounded hover:bg-gray-200 text-xl">
-                  -
-                </button>
-                <input
-                  name="quantity"
-                  type="text"
-                  placeholder="1"
-                  numeric="true"
-                  className=" w-6 text-center"
-                />
-                <button className="px-2 pb-[1px] border rounded hover:bg-gray-200 text-lg">
-                  +
-                </button>
-              </div>
+            <div className="flex justify-center gap-1">
+              <button className="px-[10px] pb-[3px] border rounded hover:bg-gray-200 text-xl max-md:text-sm">
+                -
+              </button>
+              <input
+                name="quantity"
+                type="text"
+                placeholder="1"
+                numeric="true"
+                className=" w-6 text-center max-md:text-sm"
+              />
+              <button className="px-2 pb-[1px] border rounded hover:bg-gray-200 text-lg max-md:text-sm">
+                +
+              </button>
             </div>
           </div>
         </div>
       </div>
+      <div className="w-[450px] max-md:w-[320px] h-auto flex shadow gap-4 rounded-sm pl-2 relative py-2">
+        <Skeleton classes="h-[145px] w-[120px] max-md:w-[80px] max-md:h-[120px] max-md:my-auto" />
 
-      {/* Order Summary */}
-      <div className="p-4 bg-white rounded-md shadow w-[350px] h-fit">
-        <h2 className="text-2xl font-semibold mb-4 text-center">
-          ORDER SUMMARY
-        </h2>
+        <div className="my-2">
+          <Skeleton classes="h-5 w-50 max-md:h-4 max-md:w-32 mb-2" />
+          <Skeleton classes="h-4 w-32 max-md:h-4 max-md:w-16 mb-2" />
+          <Skeleton classes="h-5 w-12 max-md:h-4 max-md:w-12" />
 
-        <div className="flex flex-col justify-between gap-2 mb-4">
-          <div>
-            <div className="flex justify-between items-center mb-1">
-              <Skeleton height={60} width={50} />
+          <div className="flex items-center max-md:text-sm my-2 gap-1">
+            <Skeleton classes="h-3 w-3 max-md:h-2 max-md:w-2" />
+            <Skeleton classes="h-3 w-3 max-md:h-2 max-md:w-2" />
+            <Skeleton classes="h-3 w-3 max-md:h-2 max-md:w-2" />
+            <Skeleton classes="h-3 w-3 max-md:h-2 max-md:w-2" />
+            <Skeleton classes="h-3 w-3 max-md:h-2 max-md:w-2" />
 
-              <div className="text-right">
-                <p>
-                  <Skeleton height={12} width={100} />
-                </p>
-                <p className="font-semibold">
-                  <Skeleton height={12} width={50} />
-                </p>
-                <p>
-                  Quantity: <Skeleton height={12} width={20} />
-                </p>
-              </div>
-            </div>
-
-            <hr />
+            <Skeleton classes="h-3 w-4 max-md:h-2 max-md:w-2" />
           </div>
 
-          <div>
-            <div className="flex justify-between items-center mb-1">
-              <Skeleton height={60} width={50} />
+          <div className="flex justify-between w-[270px] max-md:w-full max-md:gap-4">
+            <select
+              name="size"
+              className=" cursor-pointer border my-1 max-md:text-sm"
+            >
+              <option value="">Choose Size</option>
+            </select>
 
-              <div className="text-right">
-                <p>
-                  <Skeleton height={12} width={100} />
-                </p>
-                <p className="font-semibold">
-                  <Skeleton height={12} width={50} />
-                </p>
-                <p>
-                  Quantity: <Skeleton height={12} width={20} />
-                </p>
-              </div>
+            <div className="flex justify-center gap-1">
+              <button className="px-[10px] pb-[3px] border rounded hover:bg-gray-200 text-xl max-md:text-sm">
+                -
+              </button>
+              <input
+                name="quantity"
+                type="text"
+                placeholder="1"
+                numeric="true"
+                className=" w-6 text-center max-md:text-sm"
+              />
+              <button className="px-2 pb-[1px] border rounded hover:bg-gray-200 text-lg max-md:text-sm">
+                +
+              </button>
             </div>
-
-            <hr />
-          </div>
-
-          <div>
-            <div className="flex justify-between items-center mb-1">
-              <Skeleton height={60} width={50} />
-
-              <div className="text-right">
-                <p>
-                  <Skeleton height={12} width={100} />
-                </p>
-                <p className="font-semibold">
-                  <Skeleton height={12} width={50} />
-                </p>
-                <p>
-                  Quantity: <Skeleton height={12} width={20} />
-                </p>
-              </div>
-            </div>
-
-            <hr />
           </div>
         </div>
-
-        <div className="flex justify-between mb-2">
-          <span className="font-semibold">Total Items:</span>
-          <span>
-            <Skeleton height={20} width={20} />
-          </span>
-        </div>
-        <div className="flex justify-between mb-2">
-          <span className="font-semibold">Total Quantity:</span>
-          <span>
-            <Skeleton height={20} width={20} />
-          </span>
-        </div>
-        <div className="flex justify-between mb-2">
-          <span className="font-semibold">Total Price:</span>
-          <span>
-            <Skeleton height={20} width={20} />
-          </span>
-        </div>
-
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-md w-full">
-          Place Order
-        </button>
       </div>
     </div>
   );
 };
 
-export default CartLoader;
+export default CartCardLoader;
---
+++ client/src/components/cart/OrderSummaryLoader.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Skeleton } from "@ui";
+
+const OrderSummaryLoader = () => {
+  return (
+    <div className="p-4 bg-white rounded-md shadow w-[350px] h-fit">
+      <h2 className="text-2xl font-semibold mb-4 text-center">ORDER SUMMARY</h2>
+
+      <div className="flex flex-col justify-between gap-2 mb-4">
+        <div>
+          <div className="flex justify-between items-center mb-1">
+            <Skeleton classes="h-16 w-14" />
+
+            <div className="flex flex-col gap-1 items-end">
+              <Skeleton classes="h-3 w-32" />
+
+              <Skeleton classes="h-4 w-12 my-1" />
+              <div className="flex items-center justify-end gap-1">
+                Quantity: <Skeleton classes="h-3 w-3" />
+              </div>
+            </div>
+          </div>
+
+          <hr />
+        </div>
+
+        <div>
+          <div className="flex justify-between items-center mb-1">
+            <Skeleton classes="h-16 w-14" />
+
+            <div className="flex flex-col gap-1 items-end">
+              <Skeleton classes="h-3 w-32" />
+
+              <Skeleton classes="h-4 w-12 my-1" />
+              <div className="flex items-center justify-end gap-1">
+                Quantity: <Skeleton classes="h-3 w-3" />
+              </div>
+            </div>
+          </div>
+
+          <hr />
+        </div>
+      </div>
+
+      <div className="flex justify-between mb-2">
+        <span className="font-semibold">Total Items:</span>
+        <span>
+          <Skeleton classes="h-3 w-4" />
+        </span>
+      </div>
+      <div className="flex justify-between mb-2">
+        <span className="font-semibold">Total Quantity:</span>
+        <span>
+          <Skeleton classes="h-3 w-4" />
+        </span>
+      </div>
+      <div className="flex justify-between mb-2">
+        <span className="font-semibold">Total Price:</span>
+        <span>
+          <Skeleton classes="h-4 w-10" />
+        </span>
+      </div>
+
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-md w-full">
+        Place Order
+      </button>
+    </div>
+  );
+};
+
+export default OrderSummaryLoader;
