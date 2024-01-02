c6fcb8b56fca67d62dd9d247aa8e4c96a5817802
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-01-02 12:40:20 +0530

    added caching

--- client/src/pages/products/Products.jsx
+++ client/src/pages/products/Products.jsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
+import { setProducts } from "@redux";
 import { Button } from "@components/@ui";
+import memoizeData from "@utils/memoizeData";
+import { filtersConstants } from "@constants";
 import { useGetGenderFromPath } from "@hooks";
 import { LiaFilterSolid } from "react-icons/lia";
 import { useFiltersState, useProductsState } from "@hooks";
-import { getProducts, getFilters, removeFilters } from "@redux";
 import { Filters, Pagination, ProductCard, ProductLoader } from "@components";
+import { getProducts, getFilters, removeFilters, setCurrentPage } from "@redux";
 
 const Products = () => {
   const dispatch = useDispatch();
@@ -15,39 +18,47 @@ const Products = () => {
   const { gender } = useParams();
   const { filters } = useFiltersState();
   const { gender: genderFromPath } = useGetGenderFromPath();
-  const { products, status, currentPage: page } = useProductsState();
+  const { products, productsStatus, currentPage: page } = useProductsState();
 
   const [toggleFilters, setToggleFilters] = useState(false);
 
   const genderToGetProducts = gender || genderFromPath;
 
   useEffect(() => {
+    setToggleFilters(false);
     dispatch(getFilters({ gender }));
     if (!products.length > 0) {
       dispatch(getProducts({ page, gender: genderToGetProducts, filters }));
     }
-  }, [gender]);
+  }, []);
+
+  // useEffect(() => {
+  //   dispatch(getFilters({ gender }));
+  // }, [gender]);
 
   const handleApplyFiltersClick = () => {
-    dispatch(getProducts({ page, gender: genderToGetProducts, filters }));
+    dispatch(setCurrentPage(1));
+    const gender = genderToGetProducts;
+    const { isCached, cachedData } = memoizeData(1, gender, filters);
+    if (isCached) {
+      dispatch(setProducts(cachedData));
+    } else {
+      dispatch(getProducts({ page: 1, gender, filters }));
+    }
   };
 
   const handleRemoveFiltersClick = () => {
     dispatch(removeFilters());
-    const filters = {
-      rating: 0,
-      sizes: [],
-      brands: [],
-      colors: [],
-      categories: [],
-      priceRange: { min: 0, max: 0 },
-    };
-    dispatch(getProducts({ page, gender: genderToGetProducts, filters }));
+    const gender = genderToGetProducts;
+    const filters = filtersConstants;
+    const { isCached, cachedData } = memoizeData(1, gender, filters);
+    if (isCached) {
+      dispatch(setProducts(cachedData));
+      dispatch(setCurrentPage(1));
+    } else {
+      dispatch(getProducts({ page: 1, gender, filters }));
+    }
   };
-
-  useEffect(() => {
-    setToggleFilters(false);
-  }, []);
 
   const HandleToggleFilters = () => {
     setToggleFilters(!toggleFilters);
@@ -77,7 +88,7 @@ const Products = () => {
         </div>
 
         <div className="grid grid-cols-1 max-md:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 place-items-center  gap-4 mt-2">
-          {status === "loading"
+          {productsStatus === "loading"
             ? Array.from({ length: 20 }).map((_, i) => (
                 <ProductLoader key={i} />
               ))
@@ -85,8 +96,6 @@ const Products = () => {
                 <ProductCard product={product} key={product._id} />
               ))}
         </div>
-
-        {!products?.length && <div> No products found. </div>}
 
         {products?.length > 0 && <Pagination />}
       </div>
