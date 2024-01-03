36475e0949a6e50473fd4ae984af1e6cd4408b21
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-01-03 08:26:04 +0530

    removed cart key

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
@@ -20,9 +20,6 @@ const userSchema = new mongoose.Schema(
       type: String,
       default:
         "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-    },
-    cart: {
-      type: [mongoose.Schema.Types.ObjectId],
     },
   },
   { timestamps: true }
