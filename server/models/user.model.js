daa949d70c358d5eb1a47a1e5fcfaee8a7b31169
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-01-05 12:22:39 +0530

    added cart field

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -21,6 +21,9 @@ const userSchema = new mongoose.Schema(
       default:
         "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
     },
+    cart: {
+      type: [mongoose.Schema.Types.ObjectId],
+    },
   },
   { timestamps: true }
 );
