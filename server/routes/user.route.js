a0c70aabfe375a91cbe6adccee1162403ef8e979
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-01-07 12:36:22 +0530

    add avatar route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { upload } from "../middlewares/multer.middlewares.js";
 
 import { verifyToken } from "../utils/verifyToken.js";
 import {
@@ -6,6 +7,7 @@ import {
   manageCart,
   updateProfile,
   getCartProducts,
+  updateUserAvatar,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
@@ -14,5 +16,7 @@ router.get("/profile", verifyToken, getUser);
 router.post("/cart", verifyToken, manageCart);
 router.get("/cart", verifyToken, getCartProducts);
 router.post("/updateProfile", verifyToken, updateProfile);
+
+router.patch("/avatar", verifyToken, upload.single("avatar"), updateUserAvatar);
 
 export default router;
