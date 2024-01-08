dc9626ca7930a2ed0a0d0d4d9148826eedba664e
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-01-08 11:48:44 +0530

    change temp folder path

--- server/middlewares/multer.middlewares.js
+++ server/middlewares/multer.middlewares.js
@@ -1,15 +1,8 @@
 import multer from "multer";
-import fs from "fs";
-
-const destinationFolder = "public/temp";
-
-if (!fs.existsSync(destinationFolder)) {
-  fs.mkdirSync(destinationFolder, { recursive: true });
-}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, destinationFolder);
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
