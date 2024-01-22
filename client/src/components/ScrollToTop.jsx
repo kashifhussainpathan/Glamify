7f8535fce8ef45c34cf716f0ceede89c5489e307
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-01-22 13:23:53 +0530

    add currentPage dependency

--- client/src/components/ScrollToTop.jsx
+++ client/src/components/ScrollToTop.jsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { useProductsState } from "@hooks";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
+  const { currentPage } = useProductsState();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+  }, [pathname, currentPage]);
 };
 
 export default ScrollToTop;
