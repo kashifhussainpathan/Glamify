dd7d55a1fe306f692b6c5cd8d41d67ba47d0e234
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-01-23 22:16:06 +0530

    style: change button style

--- client/src/components/@ui/Button.jsx
+++ client/src/components/@ui/Button.jsx
@@ -5,7 +5,7 @@ const Button = (props) => {
     <button
       onClick={onClick}
       {...restProps}
-      className={`text-white bg-blue-700 hover:bg-blue-800 font-medium rounded text-base px-5 py-2.5 me-2 mb-2 max-md:text-xs max-md:px-3 ${className}`}
+      className={`text-white bg-gray-900 hover:bg-gray-800 font-medium rounded text-base px-5 py-2.5 mb-2 max-md:text-xs max-md:px-3 ${className}`}
     >
       {children}
     </button>
