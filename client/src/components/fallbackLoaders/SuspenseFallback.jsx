1b9e081cb91c5131c077dd986c67c358aaa4e308
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-01-24 13:43:43 +0530

    style: change style

--- client/src/components/fallbackLoaders/SuspenseFallback.jsx
+++ client/src/components/fallbackLoaders/SuspenseFallback.jsx
@@ -1,7 +1,9 @@
+import { useEffect } from "react";
+
 const SuspenseFallback = () => {
   return (
     <div
-      className={`fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-50 `}
+      className={`fixed inset-0 flex items-center justify-center bg-gray-200`}
     >
       <div className="loader"></div>
     </div>
