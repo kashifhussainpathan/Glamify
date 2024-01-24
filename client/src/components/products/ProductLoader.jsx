45c1e98d0317eb617b520bbaf2e49f621e93edab
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-01-24 10:48:56 +0530

    style: change loader style

--- client/src/components/products/ProductLoader.jsx
+++ client/src/components/products/ProductLoader.jsx
@@ -9,17 +9,17 @@ const ProductLoader = () => {
       <div className=" px-2 max-md:px-[2px]">
         <Skeleton classes="my-2 h-4 w-[160px] max-md:w-[120px] overflow-hidden overflow-ellipsis whitespace-nowrap max-md:text-[12px]" />
 
-        <Skeleton classes=" my-1 h-4 w-[140px] overflow-hidden overflow-ellipsis whitespace-nowrap max-md:text-[12px] max-md:w-[100px]" />
+        <Skeleton classes=" my-1 h-4 max-md:h-2 w-[140px] overflow-hidden overflow-ellipsis whitespace-nowrap max-md:text-[12px] max-md:w-[100px]" />
       </div>
 
       <div className=" px-2 mt-1 max-md:px-[2px]">
         <div className="flex items-center gap-1 mt-2">
-          <Skeleton classes="h-4 w-4 max-md:h-3 max:md:w-1" />
-          <Skeleton classes="h-4 w-4 max-md:h-3 max:md:w-1" />
-          <Skeleton classes="h-4 w-4 max-md:h-3 max:md:w-1" />
-          <Skeleton classes="h-4 w-4 max-md:h-3 max:md:w-1" />
+          <Skeleton classes="h-4 w-4 max-md:h-2 max-md:w-2" />
+          <Skeleton classes="h-4 w-4 max-md:h-2 max-md:w-2" />
+          <Skeleton classes="h-4 w-4 max-md:h-2 max-md:w-2" />
+          <Skeleton classes="h-4 w-4 max-md:h-2 max-md:w-2" />
 
-          <Skeleton classes="h-4 w-8 max-md:h-3 max-md:w-6" />
+          <Skeleton classes="h-4 w-8 max-md:h-2 max-md:w-4" />
         </div>
         <Skeleton classes="h-6 w-12 mt-2 max-md:h-4" />
       </div>
